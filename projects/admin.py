58d2c015cc3e59c98592e9fd4da0578bc4644dcd
louisenje <louisenje@gmail.com>
2021-05-31 08:41:33 +0300

    registered the models in admin.py file

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Profile,webapps,ratings,comment
 
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(webapps)
+admin.site.register(ratings)
+admin.site.register(comment)
